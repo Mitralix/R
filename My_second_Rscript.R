f3c20edbeea291771f8fc10c3677958ed0d2f116
Mickael <Mickaeln@uio.no>
2024-11-21 14:45:16 +0100

    funker?

--- My_second_Rscript.R
+++ My_second_Rscript.R
@@ -1,11 +1,14 @@
 library(tidyverse)
 setwd("~/")
-df <- read_csv("BIOS-IN5410/R/BIOS-IN5410/data/df1.csv")
+df <- read_csv("~/BIOS-IN5410/R/df1.csv")
 #Plot gene length vs count
 
-pdf(file = "gene_length_vs_count.pdf") 
+name = "genelength_vs_count.pdf"
+
+pdf(file = name) 
 
 #this tells R that you want to make a pdf fill under a selected name
 
-plot(df$length, df$count)
-dev.off()
+plot(df$length, df$count, col = "red")
+
+dev.off( )
